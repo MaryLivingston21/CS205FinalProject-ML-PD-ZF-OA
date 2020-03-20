0c02381a794877ae3b59fd2910f6040eba49bb5e
marylivingston21 <mliving1@uvm.edu>
2020-03-19 20:42:12 -0400

    board toString

--- src/Board.java
+++ src/Board.java
@@ -4,19 +4,50 @@ import java.util.ArrayList;
 
 public class Board {
     public ArrayList<ArrayList<Integer>> board;
+    private int boardSize = 8;
 
     public Board(){
-        initializeBoard();
+        board = initializeBoard();
     }
 
-    private ArrayList<ArrayList<Integer>> getBoard(){
+//    public static void playPiece(Player p, int r, int c){
+//        if (isValidMove(p,r,c)){
+//
+//        }
+//
+//    }
+//
+//    public static boolean isValidMove(Player p, int r, int c){
+//
+//    }
+//
+//    public static ArrayList<ArrayList<Integer>> getValidMoves(Player p){
+//
+//    }
+
+    @Override
+    public String toString(){
+        String s = "";
+        for (int i=0; i < board.size(); i++){
+            ArrayList<Integer> row = board.get(i);
+            for (int j=0; j < row.size(); j++){
+                s = s + (row.get(j) + " ");
+            }
+            s = s + "\n";
+        }
+        return s;
+    }
+
+    public  ArrayList<ArrayList<Integer>> getBoard(){
         return board;
     }
 
-    private void initializeBoard(){
+    private ArrayList<ArrayList<Integer>> initializeBoard(){
+        ArrayList<ArrayList<Integer>> board = new ArrayList<ArrayList<Integer>>();
         ArrayList<Integer> row = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0,0));
-        for (int i=0; i<8; i++){
+        for (int i=0; i<boardSize; i++){
             board.add(row);
         }
+        return board;
     }
 }
--- src/Othello.java
+++ src/Othello.java
@@ -1,4 +1,10 @@
+import java.lang.*;
+import java.util.*;
 
 public class Othello {
+    public static void main(String[] args){
+        Board game = new Board();
+        System.out.print(game);
+    }
 
 }
