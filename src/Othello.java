adae3a23776adb57d924f7fc18489bfc1ecef07a
marylivingston21 <mliving1@uvm.edu>
2020-03-23 15:32:22 -0400

    new classes

--- src/Othello.java
+++ src/Othello.java
@@ -1,18 +1,47 @@
-import java.lang.*;
-import java.util.*;
-import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
 
-public class Othello {
-    //private Game g;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+
+import javafx.geometry.Pos;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import javafx.scene.paint.Color;
+
+import javafx.scene.input.MouseEvent;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Othello extends Application {
+    private Game g;
+
+    @Override
+    public void start(Stage primaryStage){
+        //create game g = New Game()
+    }
+
+    public void handleClick(MouseEvent e) {
+
+    }
+
+
 
     public static void main(String[] args){
-        Board board = new Board();
-        System.out.print(board);
-
-        //TODO:: get numUsers, initialize users
-        ArrayList<Player> players = new ArrayList<Player>(Arrays.asList(new Player(1,"human"),new Player(2,"human")));
-
-        Game g = new Game(board, players);
+        //launch(args);
     }
 
 }
---
+++ src/OthelloBackEnd.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+import java.util.ArrayList;
+
+public class OthelloBackEnd {
+    public static void main(String[] args){
+
+        Board board = new Board();
+        System.out.print(board);
+
+        //TODO:: get numUsers, initialize users
+        ArrayList<Player> players = new ArrayList<Player>(Arrays.asList(new Player(1,"human"),new Player(2,"human")));
+
+        Game g = new Game(board, players);
+    }
+}
