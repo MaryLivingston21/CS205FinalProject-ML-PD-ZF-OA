8a9ac53a1c7a2f28b478b6fddcf31ba20d01a9ce
Patrick Dundas <patrickdundas@users.noreply.github.com>
2020-04-03 14:39:12 -0400

    Set up scanner

--- src/OthelloBackEnd.java
+++ src/OthelloBackEnd.java
@@ -1,6 +1,7 @@
 import java.lang.*;
 import java.util.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class OthelloBackEnd {
     public static void main(String[] args){
@@ -12,5 +13,9 @@ public class OthelloBackEnd {
         ArrayList<Player> players = new ArrayList<Player>(Arrays.asList(new Player(1,"human"),new Player(2,"human")));
 
         Game g = new Game(board, players);
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("")
     }
 }
